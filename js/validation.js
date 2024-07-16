451759a679f64a6589619fccb762c487f556ea49
cristianrs-dev <cris.tecwordd@gmail.com>
2024-07-16 13:41:41 -0400

    3º implementação de testes TDD com jest no formulário

--- js/validation.js
+++ js/validation.js
@@ -1,6 +1,19 @@
+
+const { JSDOM } = require('jsdom');
+
+// Criando um contexto jsdom
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
+const document = dom.window.document
+
 function campoVazio(id){
   const campo = document.getElementById(id).value.trim()
   return campo === ''
+}
+
+
+function campoEmailVazio(){
+  const email = document.getElementById("email").value.trim()
+  return email === ''
 }
 
 function verificaCamposVazios(event){
@@ -26,6 +39,6 @@ function validarEmail(email){
   return regex.test(email)
 }
 
+module.exports={validarEmail,campoVazio}
 
-
-document.getElementById("bnt-login").addEventListener('click',verificaCamposVazios)
+//document.getElementById("bnt-login").addEventListener('click',verificaCamposVazios)
